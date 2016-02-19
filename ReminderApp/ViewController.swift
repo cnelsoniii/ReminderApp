49aa35b00b4c472684ba44cbd909e0bd2de58f47
Molly Buchanan <mkb4vb@labmini10.local>
2016-02-19 01:50:22 -0500

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by Molly Buchanan on 2/19/16.
+//  Copyright © 2016 Molly Buchanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
